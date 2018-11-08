9be002091fb21bb36486e4d8a992339deb0f81f5
Yihui He 何宜晖 <yihuihe@foxmail.com>
2018-11-08 17:24:39 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -3,3 +3,5 @@ tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 cd ..
 mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
+wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+rm -f ILSVRC2012_img_val.tar
