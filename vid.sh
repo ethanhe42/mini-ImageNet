24deb0c70bfed04aa6d7e783bba887d5aa6b0c3a
Yihui He 何宜晖 <yihuihe@foxmail.com>
2018-11-09 16:37:04 -0500

    Create vid.sh

---
+++ vid.sh
@@ -0,0 +1,3 @@
+wget http://bvisionweb1.cs.unc.edu/ILSVRC2017/ILSVRC2017_VID_new.tar.gz
+wget http://bvisionweb1.cs.unc.edu/ILSVRC2017/ILSVRC2017_VID_test.tar.gz
+wget http://bvisionweb1.cs.unc.edu/ILSVRC2017/ILSVRC2017_VID.tar.gz
